5b8d02b7c1e923a17dea1b0ffdf8f4641153a11e
H44K0N <haakon.tischbein@gmail.com>
2025-09-13 19:33:27 +0200

    Initialized empty Spring Boot project

---
+++ src/main/java/no/urbanlogistikk/urbanlogistikk/UrbanlogistikkApplication.java
@@ -0,0 +1,13 @@
+package no.urbanlogistikk.urbanlogistikk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanlogistikkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanlogistikkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/urbanlogistikk/urbanlogistikk/UrbanlogistikkApplicationTests.java
@@ -0,0 +1,13 @@
+package no.urbanlogistikk.urbanlogistikk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanlogistikkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
